dc58f09587243af1a30e951d51b09c1f1389aaf9
Beastman Kojak <beastman.kojak@gmail.com>
2021-11-18 00:09:23 -0500

    add trait rarities

--- src/DerplingLookup/DerplingFilter.svelte
+++ src/DerplingLookup/DerplingFilter.svelte
@@ -1,4 +1,5 @@
 <script>
+  import DerplingDropdown from "./DerplingDropdown.svelte";
   export let baseUrl;
 
   export let aura;
@@ -22,116 +23,108 @@
 
 {#await fetchAttributes()}
   <h2>Loading...</h2>
-{:then attributes}
+{:then attrs}
   <div>
+    <p>
+      Total derplings hatched: {attrs.stats.totalCount}, last updated at {attrs
+        .stats.lastUpdate}
+    </p>
     <ul>
       <li>
-        <label for="auraDropdown">Aura</label>
-        <select bind:value={aura} id="auraDropdown">
-          <option value="">Any</option>
-          {#each attributes.aura as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={aura}
+          id="auraDropdown"
+          label="Aura"
+          attributes={attrs.attributes.aura}
+        />
       </li>
       <li>
-        <label for="beakDropdown">Beak</label>
-        <select bind:value={beak} id="beakDropdown">
-          <option value="">Any</option>
-          {#each attributes.beak as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={beak}
+          id="beakDropdown"
+          label="Beak"
+          attributes={attrs.attributes.beak}
+        />
       </li>
       <li>
-        <label for="bodyDropdown">Body</label>
-        <select bind:value={body} id="bodyDropdown">
-          <option value="">Any</option>
-          {#each attributes.body as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={body}
+          id="bodyDropdown"
+          label="Body"
+          attributes={attrs.attributes.body}
+        />
       </li>
       <li>
-        <label for="eyesDropdown">Eyes</label>
-        <select bind:value={eyes} id="eyesDropdown">
-          <option value="">Any</option>
-          {#each attributes.eyes as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={eyes}
+          id="eyesDropdown"
+          label="Eyes"
+          attributes={attrs.attributes.eyes}
+        />
       </li>
       <li>
-        <label for="headDropdown">Head</label>
-        <select bind:value={head} id="">
-          <option value="">Any</option>
-          {#each attributes.head as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={head}
+          id="headDropdown"
+          label="Head"
+          attributes={attrs.attributes.head}
+        />
       </li>
       <li>
-        <label for="cargo">Cargo</label>
-        <select bind:value={cargo} id="">
-          <option value="">Any</option>
-          {#each attributes.cargo as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={cargo}
+          id="cargoDropdown"
+          label="Cargo"
+          attributes={attrs.attributes.cargo}
+        />
       </li>
       <li>
-        <label for="colorDropdown">Color</label>
-        <select bind:value={color} id="colorDropdown">
-          <option value="">Any</option>
-          {#each attributes.color as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={color}
+          id="colorDropdown"
+          label="Color"
+          attributes={attrs.attributes.color}
+        />
       </li>
       <li>
-        <label for="genderDropdown">Sex</label>
-        <select bind:value={gender} id="genderDropdown">
-          <option value="">Any</option>
-          {#each attributes.gender as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={gender}
+          id="genderDropdown"
+          label="Sex"
+          attributes={attrs.attributes.gender}
+        />
       </li>
       <li>
-        <label for="eggshellDropdown">Eggshell</label>
-        <select bind:value={eggshell} id="eggshellDropdown">
-          <option value="">Any</option>
-          {#each attributes.eggshell as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={eggshell}
+          id="eggshellDropdown"
+          label="Eggshell"
+          attributes={attrs.attributes.eggshell}
+        />
       </li>
       <li>
-        <label for="pedestalDropdoen">Pedestal</label>
-        <select bind:value={pedestal} id="pedestalDropdown">
-          <option value="">Any</option>
-          {#each attributes.pedestal as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={pedestal}
+          id="pedestalDropdown"
+          label="Pedestal"
+          attributes={attrs.attributes.pedestal}
+        />
       </li>
       <li>
-        <label for="basecolorDropdown">Basecolor</label>
-        <select bind:value={basecolor} id="basecolorDropdown">
-          <option value="">Any</option>
-          {#each attributes.basecolor as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={basecolor}
+          id="basecolorDropdown"
+          label="Basecolor"
+          attributes={attrs.attributes.basecolor}
+        />
       </li>
       <li>
-        <label for="dadbodTagDropdown">DadbodTag</label>
-        <select bind:value={dadbodTag} id="dadbodTagDropdown">
-          <option value="">Any</option>
-          {#each attributes.dadbodTag as value}
-            <option value={encodeURIComponent(value)}>{value}</option>
-          {/each}
-        </select>
+        <DerplingDropdown
+          bind:value={dadbodTag}
+          id="dadbodTagDropdown"
+          label="Dadbod tag"
+          attributes={attrs.attributes.dadbodTag}
+        />
       </li>
     </ul>
     <button on:click>LFG</button>
