99fbbbbf0dde4ba3190d97f114fb5d96620ca584
Beastman Kojak <beastman.kojak@gmail.com>
2022-02-05 00:40:43 -0500

    Update lookup by name and cnft sale integration

--- src/Drapes/Lookup.svelte
+++ src/Drapes/Lookup.svelte
@@ -5,18 +5,18 @@
     'Fur', 'Body', 'Ears', 'Eyes', 'Head', 'Skin', 'Mouth', 'Background',
   ];
 
-  const project = "Drapes";
+  const project = "Derp Apes";
   const alt = "Drapes";
   const baseUrl =
     process.env.NODE_ENV === "prod"
       ? "https://www.beastmankojak.com/api/drapes"
       : "http://localhost:3000/drapes";
-  const assetTag = "drape";
+  const assetTag = "drapes";
   const title = "Drapes Lookup";
   const totalCount = 8888;
   const policyId = "f4873b426a498350c579690bd1f4a369d5d7b521c778acf322f77334";
   const verified = true;
-  const assetTransform = (assetName) => assetName.replace(' #', '');
+  const assetTransform = (assetName) => `DRAPE${assetName.slice(-5)}`;
   const disclaimer = 'Stats/rarities will change as more Drapes are minted';
 </script>
 
