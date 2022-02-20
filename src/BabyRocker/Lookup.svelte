53b3ee571a2596b4e4239878bf395f305604f6bf
Beastman Kojak <beastman.kojak@gmail.com>
2022-02-19 21:06:17 -0500

    Switched baby rocker lookup to cnft-svelte-components

---
+++ src/BabyRocker/Lookup.svelte
@@ -0,0 +1,40 @@
+<script>
+  import LookupApp from "@beastmankojak/cnft-svelte-components/components/LookupApp/App.svelte";
+
+  const traitList = [
+    "hat",
+    "body",
+    "eyes",
+    "mouth",
+    "clothes",
+    "accessory",
+    "background",
+  ];
+
+  const project = "BabyAlienClub Rock";
+  const alt = "Baby rocker";
+  const baseUrl =
+    process.env.NODE_ENV === "prod"
+      ? "https://www.beastmankojak.com/api/baby-rocker"
+      : "http://localhost:3000/baby-rocker";
+  const assetTag = "babyRocker";
+  const title = "Baby Rocker Lookup";
+  const totalCount = 3333;
+  const policyId = "3c1169f0336f60c46aa0651bd8130c8f35a001005cf7599bb25d7cf8";
+  const verified = true;
+  // const assetTransform = (assetName) => `BACSpaceships${assetName.slice(-5, -1)}`;
+  const placeholder = 'bac-rock-loading.png';
+</script>
+
+<LookupApp
+  {traitList}
+  {project}
+  {alt}
+  {baseUrl}
+  {assetTag}
+  {title}
+  {totalCount}
+  {policyId}
+  {verified}
+  {placeholder}
+/>
---
+++ src/BabyRocker/Rarity.svelte
@@ -0,0 +1,28 @@
+<script>
+  import RarityApp from "@beastmankojak/cnft-svelte-components/components/RarityApp/App.svelte";
+
+  const baseUrl =
+    process.env.NODE_ENV === "prod"
+      ? "https://www.beastmankojak.com/api/baby-rocker"
+      : "http://localhost:3000/baby-rocker";
+
+  const traitList = [
+    "hat",
+    "body",
+    "eyes",
+    "mouth",
+    "clothes",
+    "accessory",
+    "background",
+  ];
+
+  const totalCount = 3333;
+</script>
+
+<RarityApp
+  title="Unofficial Baby Rocker rarity chart"
+  description="This is an unofficial, community-driven Baby Rocker rarity chart."
+  {baseUrl}
+  {traitList}
+  {totalCount}
+/>
